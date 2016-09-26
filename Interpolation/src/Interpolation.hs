548357f01b08488a34ecababe21f419b15d7b598
Alexander Misonizhnik <misonijnik@gmail.com>
2016-09-26 23:59:45 +0300

    - add lagrangePolynom.

--- Interpolation/src/Interpolation.hs
+++ Interpolation/src/Interpolation.hs
@@ -35,3 +35,9 @@ lagrangePolynomK listFunc k = \x -> (omegaWK x)/(omegaWK kMonomValue)
     where kMonomValue  = negate $ (listFunc !! k) 0
           omegaWK = omegaWithoutK k listFunc
 
+lagrangePolynom :: [Node] -> Int -> ValueFunc
+lagrangePolynom listNode n = foldl1 sumFunc listTmpPolynom
+    where listNum     = [0..n]
+          cutListNode = take (n+1) listNode
+          listLagrangePolynomK = map (lagrangePolynomK (listMonomial cutListNode)) listNum 
+          listTmpPolynom = zipWith mulFunc (map (const . snd) cutListNode) listLagrangePolynomK
