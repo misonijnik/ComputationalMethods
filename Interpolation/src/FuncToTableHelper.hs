67217f3c18cb95dfdda4d57a92efc0fb907d0435
Alexander Misonizhnik <misonijnik@gmail.com>
2016-09-28 00:40:00 +0300

    - fixed stylish.

--- Interpolation/src/FuncToTableHelper.hs
+++ Interpolation/src/FuncToTableHelper.hs
@@ -1,6 +1,6 @@
 module FuncToTableHelper where
 
-import Prelude
+import           Prelude
 
 demoFunc :: ValueFunc
 --demoFunc x = 1/(1 + 25*x**2)
@@ -16,15 +16,15 @@ toDouble :: Int -> Double
 toDouble x = fromInteger $ toInteger x
 
 divisionIntoUnits :: Int -> Segment -> [Value]
-divisionIntoUnits n (a, b) = [a + h*(toDouble j) | j <- [0..n]]
-    where h = (b - a) / (toDouble n)          
+divisionIntoUnits n (a, b) = [a + h * toDouble j | j <- [0..n]]
+    where h = (b - a) / toDouble n
 
 funcToTable :: ValueFunc -> [Value] -> [Node]
 funcToTable func arr = zip arr y
     where y = map func arr
 
 deleteByIndex :: [a] -> Int -> [a]
-deleteByIndex xs n = ys ++ (tail zs)
+deleteByIndex xs n = ys ++ tail zs
     where (ys, zs) = splitAt n xs
 
 
--- Interpolation/src/Interpolation.hs
+++ Interpolation/src/Interpolation.hs
@@ -1,7 +1,7 @@
 module Interpolation where
 
-import FuncToTableHelper
-import Data.List
+import           Data.List
+import           FuncToTableHelper
 
 distanceFrom :: Value -> Node -> Node -> Ordering
 distanceFrom x (y, _) (z, _)
@@ -13,16 +13,16 @@ sortByMinDistance :: Value -> [Node] -> [Node]
 sortByMinDistance x = sortBy (distanceFrom x)
 
 monomialFrom :: Value -> ValueFunc
-monomialFrom xj = \x -> x - xj
+monomialFrom xj x = x - xj
 
 sumFunc :: ValueFunc -> ValueFunc -> ValueFunc
-sumFunc a b = \x -> a x + b x
+sumFunc a b x = a x + b x
 
 mulFunc :: ValueFunc -> ValueFunc -> ValueFunc
-mulFunc a b = \x -> a x * b x
+mulFunc a b x = a x * b x
 
 listMonomial :: [Node] -> [ValueFunc]
-listMonomial nodes = map (monomialFrom . fst) nodes
+listMonomial = map (monomialFrom . fst)
 
 omegaFrom :: [ValueFunc] -> ValueFunc
 omegaFrom = foldl1 mulFunc
@@ -31,7 +31,7 @@ omegaWithoutK :: Int -> [ValueFunc] -> ValueFunc
 omegaWithoutK k listFunc = omegaFrom $ deleteByIndex listFunc k
 
 lagrangePolynomK :: [ValueFunc] -> Int -> ValueFunc
-lagrangePolynomK listFunc k = \x -> (omegaWK x)/(omegaWK kMonomValue)
+lagrangePolynomK listFunc k x = omegaWK x / omegaWK kMonomValue
     where kMonomValue  = negate $ (listFunc !! k) 0
           omegaWK = omegaWithoutK k listFunc
 
@@ -39,5 +39,5 @@ lagrangePolynom :: [Node] -> Int -> ValueFunc
 lagrangePolynom listNode n = foldl1 sumFunc listTmpPolynom
     where listNum     = [0..n]
           cutListNode = take (n+1) listNode
-          listLagrangePolynomK = map (lagrangePolynomK (listMonomial cutListNode)) listNum 
+          listLagrangePolynomK = map (lagrangePolynomK (listMonomial cutListNode)) listNum
           listTmpPolynom = zipWith mulFunc (map (const . snd) cutListNode) listLagrangePolynomK
