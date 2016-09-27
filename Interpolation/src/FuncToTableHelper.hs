d1825a8bdcded2c2a4e3b2b6b2e5c2ecbe6e9836
Alexander Misonizhnik <misonijnik@gmail.com>
2016-09-27 04:27:40 +0300

    - fixed NonlinearEquation.cabal; - add Chart in Interpolation; - add IO in Interpolation.

--- Interpolation/src/FuncToTableHelper.hs
+++ Interpolation/src/FuncToTableHelper.hs
@@ -3,7 +3,8 @@ module FuncToTableHelper where
 import Prelude
 
 demoFunc :: ValueFunc
-demoFunc x = x**2
+--demoFunc x = 1/(1 + 25*x**2)
+demoFunc x = 1 - exp(-x) + x ** 2
 
 type Value = Double
 type Segment = (Double, Double)
@@ -12,7 +13,10 @@ type Node = (Value, Value)
 type ValueFunc = (Value -> Value)
 
 diap :: Segment
-diap = (1, 5)
+diap = (0, 1.5)
+
+numberOfValues :: Int
+numberOfValues = 15
 
 toDouble :: Int -> Double
 toDouble x = fromInteger $ toInteger x
@@ -27,4 +31,8 @@ funcToTable func arr = zip arr y
 
 deleteByIndex :: [a] -> Int -> [a]
 deleteByIndex xs n = ys ++ (tail zs)
-    where (ys, zs) = splitAt n xs 
+    where (ys, zs) = splitAt n xs
+
+
+table :: [Node]
+table = funcToTable demoFunc (divisionIntoUnits numberOfValues diap)
--- Interpolation/src/Main.hs
+++ Interpolation/src/Main.hs
@@ -1,7 +1,69 @@
 module Main where
 
 import Interpolation
+import FuncToTableHelper
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Backend.Cairo
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn "Задача алгебраического интерполирования"
+  putStrLn "Интерполяционный многочлен в форме Лагранжа"
+  putStrLn "Функция в табличной форме:"
+  mapM_ print table
+  nextWork
+  
+readAInt :: IO Int
+readAInt = readLn
+
+readADouble :: IO Double
+readADouble = readLn
+
+nextWork :: IO ()
+nextWork = do 
+    inputVal
+    putStrLn "Введите 'exit', если хотите закончить работу. Иначе нажмите 'Enter'"
+    str <- getLine
+    if str == "exit"
+        then do 
+            return ()
+        else do
+            nextWork
+
+readPower :: IO Int
+readPower = do 
+    powerN <- readAInt
+    if ((powerN > numberOfValues) || (powerN < 0))
+        then do 
+            putStrLn "Ошибка. Введите верную степень"
+            powerN <- readPower
+            return powerN
+        else do
+            return powerN
+
+inputVal :: IO ()
+inputVal = do
+    putStrLn "Введите точку 'x'"
+    pointX <- readADouble
+    putStrLn $ "Введите степень интерполяционного многочлена меньше либо равную " ++ (show numberOfValues)
+    powerN <- readPower
+    putStrLn $ "Значение интерполяционного многочлена в точке " ++ (show pointX)
+    let polynom = lagrangePolynom table powerN
+    print $ polynom pointX
+    putStrLn $ "Разница значений функции и интерполяционного многочлена в точке " ++ (show pointX)
+    print $ abs (polynom pointX - demoFunc pointX)
+    graph polynom
+
+graph :: ValueFunc -> IO ()
+graph func = toFile def "graph.png" $ do
+    layout_title .= "Графики функии и многочлена"
+    setColors [opaque blue, opaque red]
+    plot (line "Функция" [signal demoFunc listPoint])
+    plot (line "Многочлен" [signal func listPoint])
+
+
+signal :: ValueFunc -> [Value] -> [Node]
+signal func xs = [ (x, func x) | x <- xs ]
+
+listPoint :: [Value]
+listPoint = [-1,(-0.999)..1.5]
