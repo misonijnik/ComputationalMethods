5a6a36a8fde1fc8140f8937fafcafb27691be4dd
Unknown <misonijnik@gmail.com>
2016-10-15 02:19:47 +0300

    - fixed many bugs.

--- InterpolationWithoutDivision/src/InterpolationWithoutDivision.hs
+++ InterpolationWithoutDivision/src/InterpolationWithoutDivision.hs
@@ -27,7 +27,7 @@ omegaFrom = foldl1 mulFunc
 fDifferences :: [Value] -> [Value]
 fDifferences [] = []
 fDifferences [_] = []
-fDifferences (a : values) = (a' -a) : fDifferences values
+fDifferences (a : values) = (a' - a) : fDifferences values
     where a' = head values
 
 finiteDifferences :: [Node] -> Int -> [[Value]]
@@ -53,29 +53,32 @@ getInterOpt nodes x
     | x < a     = Head
     | x > b     = Tail
     | otherwise = Middle (number - 1)
-    where values = map snd nodes
+    where values = map fst nodes
           (a, b) = (head . tail $ values, last . init $ values)
-          number =   length . fst $ span (< x) values
+          number = length . fst $ span (<= x) values
 
 newtonPolynomWD :: [Node] -> Int -> ValueFunc
 newtonPolynomWD nodes n x = case interOpt of
-    Head         -> newtonPolynomWD' (newtonOmega indexMonomialHead) (map head finiteDiff) x
-    Tail         -> newtonPolynomWD' (newtonOmega indexMonomialTail) (map last finiteDiff) x
-    Middle count -> newtonPolynomWD' (newtonOmega indexMonomialMiddle) (zipWith (changeIndex count) finiteDiff indexFinite) x
-    where interOpt = getInterOpt  nodes x
+    Head         -> newtonPolynomWD' (newtonOmega indexMonomialHead) (map head finiteDiff) (x - a) / h
+    Tail         -> newtonPolynomWD' (newtonOmega indexMonomialTail) (map last finiteDiff) (x - b) / h
+    Middle count -> newtonPolynomWD' (newtonOmega indexMonomialMiddle) (zipWith (changeIndex count) finiteDiff indexFinite) (x - z count) / h
+    where interOpt = getInterOpt nodes x
           newtonOmega indexMonomial = newtonWDListFrom indexMonomial n
           finiteDiff = finiteDifferences nodes n
           indexFinite = take (n + 1) indexFiniteWD
-
+          (a, b) = (fst . head $ nodes, fst . last $ nodes)
+          h = abs ((b - a) / toDouble (length nodes - 1))
+          z index = fst $ nodes !! index
 
 newtonPolynomWD' :: [ValueFunc] -> [Value] -> ValueFunc
-newtonPolynomWD' newtonOmega finiteDiff = foldl1 sumFunc $ zipWith mulFunc newtonOmega (map const finiteDiff)
+newtonPolynomWD' newtonOmega finiteDiff = foldl1 sumFunc $ zipWith mulFunc newtonOmega (map const finiteDiff')
+    where finiteDiff' = zipWith (/) finiteDiff (1.0 : scanl1 (*) [1.0 .. toDouble (length finiteDiff - 1)]) 
 
 indexFiniteWD :: [Int]
 indexFiniteWD = [- truncate (toDouble x / 2) | x <- [0 .. ]]
 
 indexMonomialMiddle :: [Value]
-indexMonomialMiddle = [(-1) ^ x * toDouble (truncate (toDouble x / 2)) | x <- [0 .. ]]
+indexMonomialMiddle = [(-1) ^ (x + 1) * toDouble (truncate (toDouble x / 2)) | x <- [0 .. ]]
 
 indexMonomialHead :: [Value]
 indexMonomialHead = [0 .. ]
